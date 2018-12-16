4f882d5ef75a94ced384be1db63a91217020671e
Tanay Raul <tanay.raul@somaiya.edu>
2018-12-16 14:08:15 +0530

    custom admin added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['email', 'first_name', 'middle_name', 'last_name', 'is_teacher', 'is_student', 'is_superuser']
+    ordering = ['email', ]
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'address')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions', 'is_student', 'is_teacher')}),
+        (_('Important dates'), {'fields': ('last_login', )}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- core/models.py
+++ core/models.py
@@ -11,6 +11,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     middle_name = models.CharField(_('middle name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff'), default=True)
     is_teacher = models.BooleanField(_('teacher'), default=False)
     is_student = models.BooleanField(_('student'), default=True)
     address = models.TextField(_('address'), blank=True)
